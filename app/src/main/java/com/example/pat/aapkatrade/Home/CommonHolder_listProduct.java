8f22de2f2db9e54cec7cd64c1a3a99de09e9b40a
nishantmalaiya <nishantmalaiya@gmail.com>
2017-05-04 17:44:34 +0530

    nishant new

--- app/src/main/java/com/example/pat/aapkatrade/Home/CommonHolder_listProduct.java
+++ app/src/main/java/com/example/pat/aapkatrade/Home/CommonHolder_listProduct.java
@@ -17,25 +17,29 @@ import com.example.pat.aapkatrade.R;
 class CommonHolder_listProduct extends RecyclerView.ViewHolder {
 
 
-    public TextView tvProductName,tvproductprize;
-    public ImageView product_imageview,product_addcard,product_description;
+    public TextView tvProductName, tvproductprize;
+    public ImageView product_imageview, product_addcard, product_description;
     public RatingBar ratingbar;
     public CardView cardview;
-    public RelativeLayout rl_cartview;
+    public RelativeLayout rl_cartview, rl_product_addtocart, rl_product_description;
 
-    View view_grid_left,view_grid_right;
+
+    View view_grid_left, view_grid_right;
 
     public CommonHolder_listProduct(View itemView) {
         super(itemView);
-        product_addcard=(ImageView)itemView.findViewById(R.id.add_cart);
-        product_description=(ImageView)itemView.findViewById(R.id.img_product_information);
-        product_imageview=(ImageView)itemView.findViewById(R.id.img_product_image_list) ;
-        rl_cartview=(RelativeLayout)itemView.findViewById(R.id.relativelayout_cart);
-        cardview=(CardView)itemView.findViewById(R.id.cardview) ;
+        product_addcard = (ImageView) itemView.findViewById(R.id.add_cart);
+        product_description = (ImageView) itemView.findViewById(R.id.img_product_information);
+        product_imageview = (ImageView) itemView.findViewById(R.id.img_product_image_list);
+        rl_cartview = (RelativeLayout) itemView.findViewById(R.id.relativelayout_cart);
+        cardview = (CardView) itemView.findViewById(R.id.cardview);
         tvProductName = (TextView) itemView.findViewById(R.id.tv_productlist);
-        tvproductprize=(TextView) itemView.findViewById(R.id.tv_productprize);
-        view_grid_left= itemView.findViewById(R.id.view_left);
-        view_grid_right= itemView.findViewById(R.id.view_right);
+        tvproductprize = (TextView) itemView.findViewById(R.id.tv_productprize);
+        view_grid_left = itemView.findViewById(R.id.view_left);
+        view_grid_right = itemView.findViewById(R.id.view_right);
+        rl_product_description = (RelativeLayout) itemView.findViewById(R.id.rl_prt_information);
+
+        rl_product_addtocart = (RelativeLayout) itemView.findViewById(R.id.rl_add_cart);
 
 
     }
